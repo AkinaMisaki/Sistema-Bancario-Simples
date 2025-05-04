bee34e6c1b87c48d487ba1b883145f64a820c98c
Amelia Holloway <57925045+AkinaMisaki@users.noreply.github.com>
2025-05-04 15:30:00 -0300

    Adicionado Pix

--- banco.py
+++ banco.py
@@ -7,6 +7,7 @@ menu2 = 0
 menu3 = 0 
 rend = 0 
 logado = False
+valor = 0.0
 
 base_dir = os.path.dirname(__file__) # Pega o caminho do arquivo atual
 cliente_dir = os.path.join(base_dir, 'Clientes') # Cria o caminho do diretório teste dentro do diretório atual
@@ -39,7 +40,7 @@ while True:
             
             os.system('cls' if os.name == 'nt' else 'clear')
             
-            print("Menu Cliente:\n 1.Consultar saldo\n 2.Depositar\n 3.Sacar\Pix\n 4.Simular Rendimento\n 5.Voltar ao menu principal")
+            print("Menu Cliente:\n 1.Consultar saldo\n 2.Depositar\n 3.Sacar\n 4.Pix/Transferência\n 5.Simular Rendimento\n 6.Voltar ao menu principal")
             menu2=int(input("Escolha uma opção: "))
 
             if menu2==1:                                         #Opção de consulta de saque selecionada
@@ -50,54 +51,104 @@ while True:
                 input("Pressione Enter para continuar... ")
 
             if menu2==2:                                         #Opção de Deposito selecionada
-                os.system('cls' if os.name == 'nt' else 'clear')
                 with open(file_path, 'rb') as cliente:
                     infoclient = pickle.load(cliente)
-                valor = int(input("Digite o valor a ser depositado:"))
 
-                while valor <=0:                                 #Garantia que o valor selecionado não seja negativo nem zero
+                os.system('cls' if os.name == 'nt' else 'clear')
+                valor = float(input("Digite o valor a ser depositado:"))
+
+                while valor <= 0:                                #Garantia que o valor selecionado não seja negativo nem zero
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    valor = int(input("Valor negativo ou zero não suportado para deposito, digite novamente:"))
+                    valor = float(input("Valor negativo ou zero não suportado para deposito, digite novamente:"))
 
-                valatual = infoclient['saldo']
-                valatual = int(valatual)
-                valatual = valatual + valor
-                infoclient['saldo'] = valatual
+                infoclient['saldo'] += valor
+
                 with open(file_path, 'wb') as cliente:
                     pickle.dump(infoclient, cliente)
                 input("Valor depositado com sucesso, pressione enter para continuar ...")
 
-            if menu2==3:                                         #Opção de saque selecionada
+            if menu2 == 3:                                       #Opção de saque selecionada
                 os.system('cls' if os.name == 'nt' else 'clear')
                 with open(file_path, 'rb') as cliente:
                     infoclient = pickle.load(cliente)
-                valor = int(input("Digite o valor a ser sacado:"))
+                valor = float(input("Digite o valor a ser sacado:"))
 
-                while valor <=0:                                 #Garantia que o valor selecionado não seja negativo nem zero
+                while valor > infoclient['saldo']:              #Garantia que o valor selecionado não seja maior que o saldo do cliente
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    valor = int(input("Valor negativo ou zero não suportado para deposito, digite novamente:"))
+                    valor = float(input("Valor maior que saldo, digite novamente:"))
 
-                valatual = infoclient['saldo']
-                valatual = int(valatual)
-                valatual = valatual - valor
-                infoclient['saldo'] = valatual
+                while valor <= 0:                                #Garantia que o valor selecionado não seja negativo nem zero
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    valor = float(input("Valor negativo ou zero não suportado para deposito, digite novamente:"))
+    
+                infoclient['saldo'] -= valor
+
                 with open(file_path, 'wb') as cliente:
                     pickle.dump(infoclient, cliente)
                 input("Valor sacado com sucesso, pressione enter para continuar ...")
             
-            if menu2==4:                                        #Opção de rendimento selecionada
+            
+            if menu2 == 4:                                       #Opção de PIX selecionada
+                os.system('cls' if os.name == 'nt' else 'clear')
+                with open(file_path, 'rb') as cliente:
+                    infoclient = pickle.load(cliente)
+
+                pixid = int(input("Digite o número da conta para fazer pix/transferência:"))
+
+                os.system('cls' if os.name == 'nt' else 'clear')
+                pixnom = "cliente" + str(pixid) + ".pickle"
+
+                try:                                                    #Confirma se a conta do cliente existe
+                    pix_file_path = os.path.join(cliente_dir, pixnom)
+                    with open(pix_file_path, 'rb') as cliente:
+                        infopix = pickle.load(cliente)
+                except:                                                 #Caso não exista, pede para o cliente tentar novamente com um número de conta válido.
+                    print("Não há uma conta com este número. Tente novamente com um número de conta válido.")
+                    input()
+                    continue
+                
+                if infopix['id'] == infoclient['id']:                     #Garantia que o cliente não transfira para ele mesmo
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print("Não é possível transferir para você mesmo.")
+                    input()
+                    continue
+
+                dnpix = float(input("Digite o quanto irá transferir:"))
+
+                while dnpix > infoclient['saldo']:                     #Garantia que o valor selecionado não seja maior que o saldo do cliente
+                    os.system('cls' if os.name == 'nt' else 'clear')  
+                    dnpix = float(input("Valor maior que o saldo, digite novamente:"))
+                
+                while dnpix <= 0:                                #Garantia que o valor selecionado não seja negativo nem zero
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    dnpix = float(input("Valor negativo ou zero não suportado para deposito, digite novamente:"))
+
+                infoclient['saldo'] -= dnpix
+                with open(file_path, 'wb') as cliente:
+                    pickle.dump(infoclient, cliente)
+
+                infopix['saldo'] += dnpix
+                with open(pix_file_path, 'wb') as cliente:
+                    pickle.dump(infopix, cliente)
+                    
+                print("Transferência realizada com sucesso!")
+                input("Pressione enter para continuar ...")
+
+
+
+            if menu2 == 5:                                       #Opção de rendimento selecionada
                 os.system('cls' if os.name == 'nt' else 'clear')
                 with open(file_path, 'rb') as cliente:
                     infoclient = pickle.load(cliente)
                 valatual = infoclient['saldo']
-                valatual = int(valatual)
+                valatual = float(valatual)
 
                 for x in range(1, 13):
-                    valatual = valatual *1.011
+                    valatual = valatual * 1.011
                     print(f"Mês {x}, valor rendido: R${valatual}")
                 input("Pressione enter para continuar ...")
                 
-            if menu2 == 5:
+            if menu2 == 6:
                 logado = False
     
     if menu1 == 3: #encerra o programa
@@ -157,7 +208,7 @@ while True:
                     with open(file_path, 'rb') as gerente:
                         infoclient = pickle.load(gerente)
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    infoclient['saldo'] = input("Digite o novo saldo do cliente: ")
+                    infoclient['saldo'] = float(input("Digite o novo saldo do cliente: "))
                     with open(file_path, 'wb') as gerente:
                         pickle.dump(infoclient, gerente)
                     os.system('cls' if os.name == 'nt' else 'clear')
